6d124cc67c76bf910b7196776d6037f90dfaa647
cminnstockdale-droid <c.minn.stockdale@gmail.com>
2026-03-01 22:35:30 +0000

    Final version of week 8 homework

--- homework8_<catherine_stockdale>.py
+++ homework8_<catherine_stockdale>.py
@@ -5,20 +5,21 @@
 #3.	The function should print a summary of the sandwich being ordered.
 #4.	Call the function two times with a different number of arguments. 
 
-def make_sandwich(*ingredients):
-    print("\n🧾 Sandwich Order Summary:")
-    print("Your sandwich includes:")
 
+#Steps #1 & Steps #2
+def make_sandwich(*ingredients):
+    #step 3 is to print a sandwich order summary at the beginning of the list of ingredients
+    print("\n Sandwich Order Summary:")
+    
     for ingredient in ingredients:
         print(f"- {ingredient}")
 
     print("Enjoy your sandwich!\n")
 
-
 # 4. Call the function twice with different numbers of arguments
-make_sandwich("turkey", "lettuce", "tomato", "mayo")
+make_sandwich("turkey", "lettuce", "avocado", "mayo")
 
-make_sandwich("ham", "cheddar cheese", "mustard")
+make_sandwich("ham", "havarti", "mustard", "horseradish")
 
 
 
